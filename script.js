2117095bb183d99f99c5324bbd375adacbf8cb6a
ShrutiLakhani <82932172+ShrutiLakhani@users.noreply.github.com>
2021-12-08 11:42:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var textInp = document.querySelector("#text");
+var outputVal = document.querySelector("#output");
+var btnInc = document.querySelector("#btn-inc");
+var btnDec = document.querySelector("#btn-dec");
+
+btnInc.addEventListener("click", increaseFont);
+btnDec.addEventListener("click", decreaseFont);
+
+var size = 16;
+function increaseFont()
+{
+size +=2;
+var outtext = textInp.value;
+outputVal.style.fontsize = `{size}px`;
+outputVal.textContent = outtext;
+}
+
+function decreaseFont()
+{
+console.log("comes here")
+size -=2;
+var outtext = textInp.value;
+outputVal.style.fontsize = `{size}px`;
+outputVal.textContent = outtext;
+}
